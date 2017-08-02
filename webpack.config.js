9861eb721f392fd35ee62f928cb6ae1c16d25137
missyu <yyyyuan520@163.com>
2017-08-02 19:04:09 +0800

    first

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+   
+    entry: __dirname + "/app/main.js",
+    output: {
+        path: __dirname + "/build",
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.json$/,
+                loader: "json-loader"//应该是json-loader
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test:/\.css$/,
+                loader: 'style-loader!css-loader?modules'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: __dirname + "/app/index.tmpl.html"
+        })
+    ],
+    devServer: {
+        contentBase: "./public",
+        stats: { colors: true },
+        historyApiFallback: true,
+        inline: true,
+        
+    }
+}
